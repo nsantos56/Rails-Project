fd96b6da89d74fadc54d0bccdd61b182d58aa327
Nikolai V. Santos <santoskola@gmail.com>
2014-04-13 13:48:49 -0500

    Products table added

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140413184126) do
+ActiveRecord::Schema.define(version: 20140413184649) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "products", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.decimal  "price"
+    t.integer  "stock_quatity"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
