375ade0078502dd5d9be6eb044bc25e172f86635
Nikolai V. Santos <santoskola@gmail.com>
2014-04-24 13:12:46 -0500

    added model for cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,22 @@
 class OrdersController < InheritedResources::Base
+  
+  def new
+    @order = Order.new
+    @product = Product.new
+    @categories = Category.all
+  end
+  
+  def create
+    @order = Order.new(params[:order])
+    if @order.save
+      flash @order.save
+      flash[:notice] = "Successfull created order"
+      redirect_to orders_url
+    else
+      render :action => 'new'
+      #code
+    end
+    
+  end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ActiveRecord::Base
+  belongs_to :cart
+  
+  attr_accessor :card_number , :card_verification
 end
